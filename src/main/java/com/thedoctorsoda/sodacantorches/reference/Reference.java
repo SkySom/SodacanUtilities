857951be494c41b359d9d30b85c52b73710a3b00
TheDoctorSoda <Unknown>
2015-10-12 23:54:08 -0700

    Version stuff

--- src/main/java/com/thedoctorsoda/sodacantorches/reference/Reference.java
+++ src/main/java/com/thedoctorsoda/sodacantorches/reference/Reference.java
@@ -3,7 +3,7 @@ package com.thedoctorsoda.sodacantorches.reference;
 public class Reference {
 	public static final String RESOURCE_ID = "sodacantorches";
 	public static final String MOD_NAME = "Sodacan Utilities";
-	public static final String MOD_VERSION = "01.07.00";
+	public static final String MOD_VERSION = "01.07.01";
 	public static final String CLIENT_PROXY = "com.thedoctorsoda.sodacantorches.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.thedoctorsoda.sodacantorches.proxy.ServerProxy";
 }
